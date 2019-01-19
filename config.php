9897bc5ccc342fe08861fc8132de1ef0564debe0
Viktoriia Mockslenko <vika01mos@gmail.com>
2019-01-19 19:02:44 +0200

    first commit

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+$config = array(
+    'title' => "Interactive Agency"
+);
+$config2 = array(
+    'title' => "<b>Interactive</b> Agency"
+);
+$config3 = array(
+    'title' => "<p>Aliquam sagittis neque in lectus semper, nec elementum<br>
+                        arcu scelerisque. Curabitur ullamcorper auctor<br>
+                        mauris, placerat fermentum.</p>"
+);
+$config4 = array(
+    'title' => "<button type=\"button\" class=\"button1 bl3-button\">Watch more</button>"
+);
+$config5 = array(
+    'title' => "<h1>Best<b>Design</b></h1>
+                    <p>Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.<br>
+                        Curabitur ullamcorper auctor mauris, placerat fermentum lectus vulputate<br>
+                        sed. Phasellus vestibulum sit amet dolor eget eleifend. </p>"
+)
+?>
+
---
+++ header.php
@@ -0,0 +1,22 @@
+<div class="menu">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link border-menu hover-menu" href="#">Home <span
+                            class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link border-menu hover-menu" href="#">About us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link border-menu hover-menu" href="#">Offer</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link border-menu hover-menu" href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link border-menu hover-menu" href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+</div>
