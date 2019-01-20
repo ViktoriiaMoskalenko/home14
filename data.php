02fb8b648cf7495fbdd3b1086a5936b83879ec15
Viktoriia Mockslenko <vika01mos@gmail.com>
2019-01-20 17:17:02 +0200

    first commit

--- data.php
+++ data.php
@@ -1,22 +1,71 @@
 <?php
-$config = array(
-    'title' => "Interactive Agency"
+$header = array(
+    'a' => "<b>Interactive</b> Agency",
+    'div' => "            <div class=\"menu\">
+                <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+                    <ul class=\"navbar-nav mr-auto\">
+                        <li class=\"nav-item active\">
+                            <a class=\"nav-link border-menu hover-menu\" href=\"#\">Home <span
+                                        class=\"sr-only\">(current)</span></a>
+                        </li>
+                        <li class=\"nav-item\">
+                            <a class=\"nav-link border-menu hover-menu\" href=\"#\">About us</a>
+                        </li>
+                        <li class=\"nav-item\">
+                            <a class=\"nav-link border-menu hover-menu\" href=\"#\">Offer</a>
+                        </li>
+                        <li class=\"nav-item\">
+                            <a class=\"nav-link border-menu hover-menu\" href=\"#\">Portfolio</a>
+                        </li>
+                        <li class=\"nav-item\">
+                            <a class=\"nav-link border-menu hover-menu\" href=\"#\">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>",
 );
-$config2 = array(
-    'title' => "<b>Interactive</b> Agency"
+$block1 = array(
+    'div' => "<div class=\"block1-text\">
+            <h1>Lorem ipsum dolor sit amet</h1>
+            <p>Nunc vel nibh tempor, pharetra lectus congue, luctus orci.</p>
+            <button type=\"button\" class=\"button1\">Ask for price</button>
+        </div>",
+    'p' => "<p>Curabitur ullamcorper auctor<br>
+                                    mauris, placerat fermentum.</p>",
 );
-$config3 = array(
-    'title' => "<p>Aliquam sagittis neque in lectus semper, nec elementum<br>
-                        arcu scelerisque. Curabitur ullamcorper auctor<br>
-                        mauris, placerat fermentum.</p>"
+$block2 = array(
+    'div' => " <div>
+                <h1>Best<b>Design</b></h1>
+                <p>Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.<br>
+                    Curabitur ullamcorper auctor mauris, placerat fermentum lectus vulputate<br>
+                    sed. Phasellus vestibulum sit amet dolor eget eleifend. </p>
+                <button type=\"button\" class=\"button1\">Ask for price</button>
+            </div>",
+    'p' => "<p>Aliquam sagittis neque in lectus semper,<br>
+                        nec elementum arcu scelerisque. Curabitur<br>
+                        ullamcorper auctor mauris, placerat fermentum.</p>",
+    'name' => array(
+        'h2' => " <h2>John Doe</h2>",
+        'p' => " <div>
+        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.<br>
+            Try awsome tool for desgners <a href=\"#\">symu.co</a></p>
+    </div>",
+        'div' => "<button type=\"button\" class=\"button1\">Watch more</button>",
+    ),
 );
-$config4 = array(
-    'title' => "<button type=\"button\" class=\"button1 bl3-button\">Watch more</button>"
-);
-$config5 = array(
-    'title' => "<h1>Best<b>Design</b></h1>
-                    <p>Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.<br>
-                        Curabitur ullamcorper auctor mauris, placerat fermentum lectus vulputate<br>
-                        sed. Phasellus vestibulum sit amet dolor eget eleifend. </p>"
+$block3 = array(
+    'p1' => array(
+        'p' => "<p><b>Web design</b></p>",
+    ),
+    'p2' => array(
+        'p' => "<p><b>Web Applications</b></p>",
+    ),
+    'p3' => array(
+        'p' => "<p><b>Digital paiting</b></p>",
+    ),
+    'p4' => array(
+        'p' => "<p><b>Desktop Applications</b></p>",
+    ),
+    'h1' => " <h1>Neptune template</h1>",
 )
 ?>
--- index.php
+++ index.php
@@ -2,221 +2,203 @@
 require "data.php";
 ?>
 
-<DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title><?php echo $config['title']; ?></title>
-        <link rel="stylesheet" href="assets/css/main.css">
-    </head>
-    <body>
-    <header>
-        <div class="container1">
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="logo-menu">
-                    <a class="navbar-brand" href="/"><?php echo $config2['title']; ?></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <?php include "header.php" ?>
-            </nav>
-            <div class="block1-text">
-                <h1>Lorem ipsum dolor sit amet</h1>
-                <p>Nunc vel nibh tempor, pharetra lectus congue, luctus orci.</p>
-                <?php echo $config4['title']; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+<header>
+    <div class="container1">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="logo-menu">
+                <a class="navbar-brand" href="/"><?php echo $header ['a'] ?></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
             </div>
-            <div class="block1-img">
-                <a href="#"><img src="src/img/scroll.png"></a>
-            </div>
+            <?php echo $header ['div'] ?>
+        </nav>
+        <?php echo $block1 ['div'] ?>
+        <div class="block1-img">
+            <a href="#"><img src="src/img/scroll.png"></a>
         </div>
-    </header>
-    <section>
-        <div class="block1-bg3">
-            <div class="block1-img1">
-                <div>
-                    <?php echo $config5['title']; ?>
-                    <?php echo $config4['title']; ?>
-                </div>
-            </div>
-            <div class="block1-img2">
-                <div>
-                    <?php echo $config5['title']; ?>
-                    <?php echo $config4['title']; ?>
-                </div>
-            </div>
+    </div>
+</header>
+<section>
+    <div class="block1-bg3">
+        <div class="block1-img1">
+            <?php echo $block2 ['div'] ?>
         </div>
-    </section>
-    <section>
-        <div class="container1">
-            <div>
-                <h1>What we do?</h1>
-            </div>
-            <div class="block2-content">
-                <div class="block2 bl2-1">
-                    <div>
-                        <a href="#"><img src="src/img/11.png"></a>
-                    </div>
-                    <div class="bl1">
-                        <p><b>Web design</b></p>
-                        <?php echo $config3['title']; ?>
-                    </div>
-                </div>
-                <div class="block2 bl2-2">
-                    <div>
-                        <a href="#"><img src="src/img/22.png"></a>
-                    </div>
-                    <div class="bl1">
-                        <p><b>Web Applications</b></p>
-                        <?php echo $config3['title']; ?>
-                    </div>
-                </div>
-            </div>
-            <div class="block2-content">
-                <div class="block2">
-                    <div>
-                        <a href="#"><img src="src/img/33.png"></a>
-                    </div>
-                    <div class="bl1">
-                        <p><b>Digital paiting</b></p>
-                        <?php echo $config3['title']; ?>
-                    </div>
-                </div>
-                <div class="block2">
-                    <div>
-                        <a href="#"><img src="src/img/44.png"></a>
-                    </div>
-                    <div class="bl1">
-                        <p><b>Desktop Applications</b></p>
-                        <?php echo $config3['title']; ?>
-                    </div>
-                </div>
-            </div>
-            <div class="button-block2">
-                <?php echo $config4['title']; ?>
-            </div>
+        <div class="block1-img2">
+            <?php echo $block2 ['div'] ?>
         </div>
-    </section>
-    <section>
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="bl3">
-                        <div class="bl3-1"></div>
-                        <div class="bl3-2">
-                            <h1>Neptune template</h1>
-                            <?php echo $config3['title']; ?>
-                            <button type="button" class="button1 bl3-button">Watch more</button>
-                        </div>
-                        <div class="bl3-3"></div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="bl3">
-                        <div class="bl3-3"></div>
-                        <div class="bl3-2">
-                            <h1>Neptune template</h1>
-                            <?php echo $config3['title'];
-                            echo $config4['title']; ?>
-                        </div>
-                        <div class="bl3-1"></div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="bl3">
-                        <div class="bl3-1"></div>
-                        <div class="bl3-3"></div>
-                        <div class="bl3-2">
-                            <h1>Neptune template</h1>
-                            <?php echo $config3['title'];
-                            echo $config4['title']; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </section>
-    <section>
-        <div class="bg-block4">
-            <div class="container1">
-                <div class="block4-headline">
-                    <h2>About us!</h2>
-                    <?php echo $config3['title']; ?>
-                </div>
-                <div class="block4">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <ul>
-                                <li>
-                                    <a href="#"><img src="src/img/p1.png"></a>
-                                    <h2>John Doe</h2>
-                                    <p>Curabitur ullamcorper auctor<br>
-                                        mauris, placerat fermentum.</p>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-md-3">
-                            <ul>
-                                <li>
-                                    <a href="#"><img src="src/img/p2.png"></a>
-                                    <h2>John Doe</h2>
-                                    <p>Aliquam sagittis neque in lectus<br>
-                                        semper, arcu scelerisque. </p>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-md-3">
-                            <ul>
-                                <li>
-                                    <a href="#"><img src="src/img/p3.png"></a>
-                                    <h2>John Doe</h2>
-                                    <p>Curabitur ullamcorper auctor<br>
-                                        mauris, placerat fermentum.</p>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-md-3">
-                            <ul>
-                                <li>
-                                    <a href="#"><img src="src/img/p4.png"></a>
-                                    <h2>John Doe</h2>
-                                    <p>Neque in lectus semper, nec<br>
-                                        elementum arcu scelerisque. </p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="button-bl4">
-                        <button type="button" class="button1">Watch more</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section>
-        <div class="bg-block5">
-            <div class="location">
-                <img src="src/img/pin.png"></div>
-        </div>
-    </section>
-    <footer>
+    </div>
+</section>
+<section>
+    <div class="container1">
         <div>
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.<br>
-                Try awsome tool for desgners <a href="#">symu.co</a></p>
+            <h1>What we do?</h1>
         </div>
-    </footer>
-    <script src="assets/js/libs.js"></script>
-    <script src="assets/js/main.js"></script>
-    </body>
-    </html>
-
+        <div class="block2-content">
+            <div class="block2 bl2-1">
+                <div>
+                    <a href="#"><img src="src/img/11.png"></a>
+                </div>
+                <div class="bl1">
+                    <?php echo $block3 ['p1'] ['p'] ?>
+                    <?php echo $block2 ['p'] ?>
+                </div>
+            </div>
+            <div class="block2 bl2-2">
+                <div>
+                    <a href="#"><img src="src/img/22.png"></a>
+                </div>
+                <div class="bl1">
+                    <?php echo $block3 ['p2'] ['p'] ?>
+                    <?php echo $block2 ['p'] ?>
+                </div>
+            </div>
+        </div>
+        <div class="block2-content">
+            <div class="block2">
+                <div>
+                    <a href="#"><img src="src/img/33.png"></a>
+                </div>
+                <div class="bl1">
+                    <?php echo $block3 ['p3'] ['p'] ?>
+                    <?php echo $block2 ['p'] ?>
+                </div>
+            </div>
+            <div class="block2">
+                <div>
+                    <a href="#"><img src="src/img/44.png"></a>
+                </div>
+                <div class="bl1">
+                    <p><?php echo $block3 ['p4'] ['p'] ?>
+                        <?php echo $block2 ['p'] ?>
+                </div>
+            </div>
+        </div>
+        <div class="button-block2">
+            <?php echo $block2 ['name'] ['div'] ?>
+        </div>
+    </div>
+</section>
+<section>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="bl3">
+                    <div class="bl3-1"></div>
+                    <div class="bl3-2">
+                        <?php echo $block3 ['h1'] ?>
+                        <?php echo $block2 ['p'] ?>
+                        <?php echo $block2 ['name'] ['div'] ?>
+                    </div>
+                    <div class="bl3-3"></div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="bl3">
+                    <div class="bl3-3"></div>
+                    <div class="bl3-2">
+                        <?php echo $block3 ['h1'] ?>
+                        <?php echo $block2 ['p'] ?>
+                        <?php echo $block2 ['name'] ['div'] ?>
+                    </div>
+                    <div class="bl3-1"></div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="bl3">
+                    <div class="bl3-1"></div>
+                    <div class="bl3-3"></div>
+                    <div class="bl3-2">
+                        <?php echo $block3 ['h1'] ?>
+                        <?php echo $block2 ['p'] ?>
+                        <?php echo $block2 ['name'] ['div'] ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</section>
+<section>
+    <div class="bg-block4">
+        <div class="container1">
+            <div class="block4-headline">
+                <h2>About us!</h2>
+                <?php echo $block2 ['p'] ?>
+            </div>
+            <div class="block4">
+                <div class="row">
+                    <div class="col-md-3">
+                        <ul>
+                            <li>
+                                <a href="#"><img src="src/img/p1.png"></a>
+                                <?php echo $block2 ['name'] ['h2'] ?>
+                                <?php echo $block1 ['p'] ?>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <ul>
+                            <li>
+                                <a href="#"><img src="src/img/p2.png"></a>
+                                <?php echo $block2 ['name'] ['h2'] ?>
+                                <?php echo $block1 ['p'] ?>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <ul>
+                            <li>
+                                <a href="#"><img src="src/img/p3.png"></a>
+                                <?php echo $block2 ['name'] ['h2'] ?>
+                                <?php echo $block1 ['p'] ?>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <ul>
+                            <li>
+                                <a href="#"><img src="src/img/p4.png"></a>
+                                <?php echo $block2 ['name'] ['h2'] ?>
+                                <?php echo $block1 ['p'] ?>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="button-bl4">
+                    <?php echo $block2 ['name'] ['div'] ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="bg-block5">
+        <div class="location">
+            <img src="src/img/pin.png"></div>
+    </div>
+</section>
+<footer>
+    <?php echo $block2 ['name'] ['p'] ?>
+</footer>
+<script src="assets/js/libs.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
